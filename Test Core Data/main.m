c3bbbe3304aab2214106502b22e3713a57c2f2f6
Ricardo Champa <ricardochampa@gmail.com>
2016-04-23 21:14:14 +0200

    Initial Commit

---
+++ Test Core Data/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test Core Data
+//
+//  Created by Ricardo Champa on 23/04/16.
+//  Copyright © 2016 Clickmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
